073fea4caa15e9f9813293634076cc45a915c57a
balaguero <juan.server2004@gmail.com>
2015-10-29 20:01:58 -0300

    Implement filter on js files

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 (function(){
 	angular
 		.module("gemStore", [])
-		.controller("StoreController", ['$scope', function($scope){
+		.controller("StoreController", ['$scope', 'caseFilter', function($scope, caseFilter){
 			var that = this;
 
             that.logo = "img/angularjs-logo.png";
             that.link = "https://angularjs.org/";
 
 			that.gems = [
-				{name: 'Azurite', price: 2.95, canPurchase: true, sold: 0, soldOut: false},
+				{name: caseFilter('Azurite', 'upper'), price: 2.95, canPurchase: true, sold: 0, soldOut: false},
 				{name: 'Amarillite', price: 3.95, canPurchase: true, sold: 0, soldOut: false},
 				{name: 'Zafire', price: 6.95, canPurchase: false, sold: 0, soldOut: false},
 				{name: 'Bloodstone', price: 2.95, canPurchase: true, sold: 0, soldOut: true}
@@ -22,7 +22,7 @@
 				gem.sold --;
 			};
 		}])
-        .filter('caseFilter', function(){
+        .filter('case', function(){
             return function(input, caseType) {
                 if (caseType.toLowerCase() === "upper") return input.toUpperCase();
                 if (caseType.toLowerCase() === "upper") return input.toLowerCase();
