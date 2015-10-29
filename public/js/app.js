c8a0b56072e0837050a9c296004ada2170d85da6
balaguero <juan.server2004@gmail.com>
2015-10-29 18:49:15 -0300

    Add ng-bind directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,22 @@
 (function(){
-	angular.module("gemStore", [])
-	.controller("StoreController", function($scope){
-		var that = this;
+	angular
+		.module("gemStore", [])
+		.controller("StoreController", ['$scope', function($scope){
+			var that = this;
 
-		that.gems = [
-			{name: 'Azurite', price: 2.95, canPurchase: true, sold: 0, soldOut: false},
-			{name: 'Amarillite', price: 3.95, canPurchase: true, sold: 0, soldOut: false},
-			{name: 'Zafire', price: 6.95, canPurchase: false, sold: 0, soldOut: false},
-			{name: 'Bloodstone', price: 2.95, canPurchase: true, sold: 0, soldOut: true}
-		];
+			that.gems = [
+				{name: 'Azurite', price: 2.95, canPurchase: true, sold: 0, soldOut: false},
+				{name: 'Amarillite', price: 3.95, canPurchase: true, sold: 0, soldOut: false},
+				{name: 'Zafire', price: 6.95, canPurchase: false, sold: 0, soldOut: false},
+				{name: 'Bloodstone', price: 2.95, canPurchase: true, sold: 0, soldOut: true}
+			];
 
-		that.purchaseGem = function(gem){
-			gem.sold ++;
-		};
+			that.purchaseGem = function(gem){
+				gem.sold ++;
+			};
 
-		that.refundGem = function(gem){
-			gem.sold --;
-		}
-	});
+			that.refundGem = function(gem){
+				gem.sold --;
+			};
+		}]);
 })();
