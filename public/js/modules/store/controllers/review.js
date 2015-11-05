acbe4a6c3e1de6ea378ba375bde717ebe89ae5df
balaguero <juan.server2004@gmail.com>
2015-11-05 18:23:41 -0300

    Ex 4, fix missing author on reviews

--- public/js/modules/store/controllers/review.js
+++ public/js/modules/store/controllers/review.js
@@ -8,11 +8,11 @@
 		that.resetNewReview = resetNewReview
 
 		that.availableStars = [
-			{label: 1, value: 1},
-			{label: 2, value: 2},
-			{label: 3, value: 3},
-			{label: 4, value: 4},
-			{label: 5, value: 5}
+			{label: '1 Star', value: 1},
+			{label: '2 Stars', value: 2},
+			{label: '3 Stars', value: 3},
+			{label: '4 Stars', value: 4},
+			{label: '5 Stars', value: 5}
 		];
 
 		that.addReview = addReview;
